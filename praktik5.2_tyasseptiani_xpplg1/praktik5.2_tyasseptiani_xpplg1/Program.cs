6bc905e6d2543926705c45e1a0368918b8f8c1ae
septiani258738-dev <septiani258738@student.smkn1kandeman.sch.id>
2025-10-13 13:27:25 +0700

    Add files via upload

---
+++ praktik5.2_tyasseptiani_xpplg1/praktik5.2_tyasseptiani_xpplg1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace praktik5._2_tyasseptiani_xpplg1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
